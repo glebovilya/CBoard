60ddbdea7115aa9604bd31561c657983b252d0f6
Stepanjuk <stepanjuk@nixsolutions.com>
2013-11-27 19:04:47 +0200

    modified my .js

--- js/popoverForEmployee.js
+++ js/popoverForEmployee.js
@@ -1,4 +1,10 @@
 /**
  * Created by stepanjuk on 27.11.13.
  */
-$(".employee-body").popover({trigger:"hover", content: "project, project, birthday, some information"})
+$(".employee-body").popover({trigger:"hover", content: "project, project, birthday, some information"});
+//var divPeople = $("#people");
+//$("#people .list-item").({trigger:"click",placement:"top", content: "888888888888999999999999999999hjiiiiiiiii"});
+$("#people .list-item").hover(function(event){
+    console.log(event.target);
+    console.log(event.target.attr("href"));
+});

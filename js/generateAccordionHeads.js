6fe76f93adc2d9f109b93578f93fc61ee7f40d43
Bogush Tatyana <tatyanabogush@gmail.com>
2013-11-27 13:49:00 +0200

    working style

---
+++ js/generateAccordionHeads.js
@@ -0,0 +1,13 @@
+var template = '<div class="accordion-group">
+<div class="accordion-heading">
+
+    <button type="button" class="btn accord btn-custom accordion-toggle"
+    data-toggle="collapse"
+    data-parent="#accordion-people" href="#collapseOne">
+    simple collapsible1
+    </button>
+
+    </div>
+
+</div>'
+

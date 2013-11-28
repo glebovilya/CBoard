7190bdafd92074a8a0c5878d4916ed7e85040aae
Stepanjuk <stepanjuk@nixsolutions.com>
2013-11-28 09:47:12 +0200

    nathing

--- js/popoverForEmployee.js
+++ js/popoverForEmployee.js
@@ -1,7 +1,7 @@
 /**
  * Created by stepanjuk on 27.11.13.
  */
-$(".employee-body").popover({trigger:"hover", content: "project, project, birthday, some information"});
+$(".employee-body").popover({trigger:"hover", content: $("<img src='../img/border-img2.png' />")});
 //var divPeople = $("#people");
 //$("#people .list-item").({trigger:"click",placement:"top", content: "888888888888999999999999999999hjiiiiiiiii"});
 $("#people .list-item").hover(function(event){

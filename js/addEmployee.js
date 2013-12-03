1ce037fe6d7c8d2817ba2c35e63025e7a40959e6
Stepanjuk <stepanjuk@nixsolutions.com>
2013-12-03 16:34:54 +0200

    modified:   js/addEmployee.js 	modified:   templates/addEmployee.html

--- js/addEmployee.js
+++ js/addEmployee.js
@@ -2,45 +2,16 @@
  * Created by stepanjuk on 29.11.13.
  */
 
-require(['stepa','Accordion'], function(){
-
-// прицепить на ининциирующую кнопку  id ="buttonAddNewPeople"
-//    console.log($("#buttonAddPeople"));
-//    $("#buttonAddPeople").on("click", function(){console.log("yep")});
-
-     $("#buttonAddNewPeople").click(function(event){
-//            console.log(event.target);
-
-            var template = '<div id="modalAddPeople" class="modal"\
-            role="dialog"\
-            aria-labelledby="myModalLabel"\
-            aria-hidden="true"\
-            data-backdrop = "false"\
-            style="display: none;">\
-                <form action="/upload_person" method="post" enctype="multipart/form-data" id="addperson">\
-                    <div class="modal-header" style="height: 20px">\
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
-                    </div>\
-                    <div class="modal-body pull-right" >\
-                        <input id="nameAddPeople" class="input-medium" type="text" name="name" placeholder="name">\
-                        <input id="surnameAddPeople" class="input-medium" type="text" name="surname" placeholder="surname">\
-                        <input id="positionAddPeople" class="input-medium" type="text" name="position" placeholder="position">\
-                                    <br/>\
-                        <span> &nbsp;&nbsp;add a photo &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>\
-                        <input id="photoAddPeople" type="file" name="photo" class="btn " multiple="multiple">\
-                    </div>\
-                    <div class="modal-footer">\
-                                        <!--<button class="btn btn-primary">Save changes</button> если не хотим чтоб кнопка закрывала окно -->\
-                         <button type= "submit" class="btn btn-custom" id="ajaxAddNewPeople">Add employee</button>\
-                    </div>\
-                </form>\
-            </div>'
-
-                 $(template).appendTo($("#inner-board"));
+require([/*'stepa','Accordion',*/'text!../templates/addEmployee.html'], function(templ){
 
 
+    var template = templ;
 
-            $('#modalAddPeople form').submit(function(){ //listen for submit event
+    $("#buttonAddNewPeople").click(function(event){
+
+    $(template).appendTo($("#inner-board"));
+
+    $('#modalAddPeople form').submit(function(){ //listen for submit event
 
                 var formData = new FormData($(this)[0]);
 
@@ -54,26 +25,48 @@ require(['stepa','Accordion'], function(){
                     processData: false,
                     success: function (returndata) {
                         onAjaxSuccess(returndata);
-                        console.log(returndata)
+//                        console.log(returndata)
                     }
                 });
 
                 return false;
 
-            });
+    });
 
-            $("#modalAddPeople .close").on('click', function(){
+    $("#modalAddPeople .close").on('click', function(){
                 $("#modalAddPeople").remove();
-            })
+    });
 
-
-
-
-
-            function onAjaxSuccess(data){
+    function onAjaxSuccess(data){// по приходу колбэка после сохранения нового сотрудника
 
                 $("#modalAddPeople :input").val("");
+
              }
 
-     });
+    });
 });
+
+//            var template = '<div id="modalAddPeople" class="modal"\
+//            role="dialog"\
+//            aria-labelledby="myModalLabel"\
+//            aria-hidden="true"\
+//            data-backdrop = "false"\
+//            style="display: none;">\
+//                <form action="/upload_person" method="post" enctype="multipart/form-data" id="addperson">\
+//                    <div class="modal-header" style="height: 20px">\
+//                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
+//                    </div>\
+//                    <div class="modal-body pull-right" >\
+//                        <input id="nameAddPeople" class="input-medium" type="text" name="name" placeholder="name">\
+//                        <input id="surnameAddPeople" class="input-medium" type="text" name="surname" placeholder="surname">\
+//                        <input id="positionAddPeople" class="input-medium" type="text" name="position" placeholder="position">\
+//                                    <br/>\
+//                        <span> &nbsp;&nbsp;add a photo &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>\
+//                        <input id="photoAddPeople" type="file" name="photo" class="btn " multiple="multiple">\
+//                    </div>\
+//                    <div class="modal-footer">\
+//                                        <!--<button class="btn btn-primary">Save changes</button> если не хотим чтоб кнопка закрывала окно -->\
+//                         <button type= "submit" class="btn btn-custom" id="ajaxAddNewPeople">Add employee</button>\
+//                    </div>\
+//                </form>\
+//            </div>'

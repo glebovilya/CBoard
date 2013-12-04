8b4bd5cd9700bdc817d4eae764f255d340bad849
Stepanjuk <stepanjuk@nixsolutions.com>
2013-12-04 14:29:39 +0200

    modified:   js/Classes/Person.js 	modified:   js/addProject.js 	modified:   js/confirmDate.js 	modified:   js/newEmployee.js 	modified:   js/stepa.js 	modified:   templates/addProject.html 	modified:   templates/addRemoveDate.html 	modified:   templates/employe.html

--- js/confirmDate.js
+++ js/confirmDate.js
@@ -2,7 +2,7 @@
  * Created by stepanjuk on 02.12.13.
  */
 
-require(['text!../templates/addRemoveDate.html','newEmployee', 'bootstrap-datepicker'], function(templ){
+require(['text!../templates/addRemoveDate.html','text!../templates/employe.html', 'bootstrap-datepicker'], function(templ,templEmploye){
 
 
 
@@ -10,34 +10,35 @@ require(['text!../templates/addRemoveDate.html','newEmployee', 'bootstrap-datepi
 
 
 
-var template = '<div id="myModal" class="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
-    <div class="modal-header" style="height: 20px">\
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
-        <h3 id="myModalLabel"></h3>\
-    </div>\
-        <div class="modal-body" >\
-        <div class = "row-fluid">\
-            <div class="span3">\
-            <form class="form-horizontal" action="" method="post">\
-                <input class="input-xlarge" type="text" placeholder="enter the name of the project">\
-                <div></br></div>\
-                    <input class="input-xlarge" type="text" placeholder="enter the name of the project">\
-                    <div></br></div>\
-                        <div class="input-append date" id="add"  data-date-format="dd-mm-yyyy">\
-                    <input  size="16"  class = "datepicker" data-provide="datepicker" type="text"  placeholder="Date of joining the project">\
-                                <span class="add-on "><i class="icon-th"></i></span>\
-                                <div></br></div>\
-            </div>\
-            </form>\
-                            </div>\
-                           <div class=" span3 pull-right windowForPhoto">\
-                            </div>\
-                        </div>\
-                        <div class="modal-footer">\
-                            <!--<button class="btn btn-primary">Save changes</button> если не хотим чтоб кнопка закрывала окно -->\
-                            <button class="btn btn-custom" data-dismiss="modal" aria-hidden="true">Save changes</button>\
-                        </div>\
-                    </div>';
+var template = templ;
+//    '<div id="myModal" class="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
+//    <div class="modal-header" style="height: 20px">\
+//        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
+//        <h3 id="myModalLabel"></h3>\
+//    </div>\
+//        <div class="modal-body" >\
+//        <div class = "row-fluid">\
+//            <div class="span3">\
+//            <form class="form-horizontal" action="" method="post">\
+//                <input class="input-xlarge" type="text" placeholder="enter the name of the project">\
+//                <div></br></div>\
+//                    <input class="input-xlarge" type="text" placeholder="enter the name of the project">\
+//                    <div></br></div>\
+//                        <div class="input-append date" id="add"  data-date-format="dd-mm-yyyy">\
+//                    <input  size="16"  class = "datepicker" data-provide="datepicker" type="text"  placeholder="Date of joining the project">\
+//                                <span class="add-on "><i class="icon-th"></i></span>\
+//                                <div></br></div>\
+//            </div>\
+//            </form>\
+//                            </div>\
+//                           <div class=" span3 pull-right windowForPhoto">\
+//                            </div>\
+//                        </div>\
+//                        <div class="modal-footer">\
+//                            <!--<button class="btn btn-primary">Save changes</button> если не хотим чтоб кнопка закрывала окно -->\
+//                            <button class="btn btn-custom" data-dismiss="modal" aria-hidden="true">Save changes</button>\
+//                        </div>\
+//                    </div>';
 
 
 
@@ -47,22 +48,23 @@ var template = '<div id="myModal" class="modal" tabindex="-1" role="dialog" aria
 
 
 
-var templateEmployee = ' <div class = "employee">\
-        <div class="employeeWindow ">\
-            <div class="employee-header" >\
-                <div >position</div>\
-                <button type="button" class="close" data-toggle="tooltip" title="remove from project"  aria-hidden="true" >&times;</button>\
-            </div>\
-            <div class="employee-body">\
-                <div class="united" >\
-                    <img src=' + empl.photo + ' alt="">\
-                        <div class= "name" >'+empl.name +'</br>'+empl.surname +'</div>\
-                </div>\
-            </div>\
-            <div class="employee-footer">\
-            </div>\
-        </div>\
-        </div>';
+var templateEmployee = templEmploye;
+//    ' <div class = "employee">\
+//        <div class="employeeWindow ">\
+//            <div class="employee-header" >\
+//                <div >position</div>\
+//                <button type="button" class="close" data-toggle="tooltip" title="remove from project"  aria-hidden="true" >&times;</button>\
+//            </div>\
+//            <div class="employee-body">\
+//                <div class="united" >\
+//                    <img src=' + empl.photo + ' alt="">\
+//                        <div class= "name" >'+empl.name +'</br>'+empl.surname +'</div>\
+//                </div>\
+//            </div>\
+//            <div class="employee-footer">\
+//            </div>\
+//        </div>\
+//        </div>';
 
 
 
@@ -74,7 +76,7 @@ var templateEmployee = ' <div class = "employee">\
 
 
         $("#formConfirmDate").ready(function(){
-            console.log("yep");
+
              $(".datepicker").datepicker();
 
             $(templateEmployee).appendTo($(".windowForPhoto"));

1c99fab99d5cb6b63f7fb8c9b0ee7d56314e9852
Stepanjuk <stepanjuk@nixsolutions.com>
2013-12-04 17:23:24 +0200

    clear

--- public/javascripts/requier/addEmployee.js
+++ public/javascripts/requier/addEmployee.js
@@ -2,41 +2,12 @@
  * Created by stepanjuk on 29.11.13.
  */
 
-require([/*'stepa','Accordion',*/'text!../templates/addEmployee.html'], function(templ){
+require([/*'stepa','Accordion',*/'text!./templates/addEmployee.html'], function(templ){
 
-// прицепить на ининциирующую кнопку  id ="buttonAddNewPeople"
-//    console.log($("#buttonAddPeople"));
-//    $("#buttonAddPeople").on("click", function(){console.log("yep")});
 
     var template = templ;
-     $("#buttonAddNewPeople").click(function(event){
-//            console.log(event.target);
 
     $("#buttonAddNewPeople").click(function(event){
-            var template = '<div id="modalAddPeople" class="modal"\
-            role="dialog"\
-            aria-labelledby="myModalLabel"\
-            aria-hidden="true"\
-            data-backdrop = "false"\
-            style="display: none;">\
-                <form action="/user" method="post" enctype="multipart/form-data" id="">\
-                    <div class="modal-header" style="height: 20px">\
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
-                    </div>\
-                    <div class="modal-body pull-right" >\
-                        <input id="nameAddPeople" class="input-medium" type="text" name="name" placeholder="name">\
-                        <input id="surnameAddPeople" class="input-medium" type="text" name="surname" placeholder="surname">\
-                        <input id="positionAddPeople" class="input-medium" type="text" name="position" placeholder="position">\
-                                    <br/>\
-                        <span> &nbsp;&nbsp;add a photo &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>\
-                        <input id="photoAddPeople" type="file" name="photo" class="btn">\
-                    </div>\
-                    <div class="modal-footer">\
-                                        <!--<button class="btn btn-primary">Save changes</button> если не хотим чтоб кнопка закрывала окно -->\
-                         <button type= "submit" class="btn btn-custom" id="ajaxAddNewPeople">Add employee</button>\
-                    </div>\
-                </form>\
-            </div>'
 
     $(template).appendTo($("#inner-board"));
 
@@ -45,7 +16,7 @@ require([/*'stepa','Accordion',*/'text!../templates/addEmployee.html'], function
                 var formData = new FormData($(this)[0]);
 
                 $.ajax({
-                    url: '/upload_person',
+                    url: '/user',
                     type: 'POST',
                     data: formData,
                     async: false,
@@ -54,19 +25,13 @@ require([/*'stepa','Accordion',*/'text!../templates/addEmployee.html'], function
                     processData: false,
                     success: function (returndata) {
                         onAjaxSuccess(returndata);
-//                        console.log(returndata)
+                        console.log(returndata)
                     }
                 });
 
                 return false;
 
     });
-                    $('<input />').attr('type', 'hidden')
-                        .attr('name', 'method')
-                        .attr('value', 'add')
-                        .appendTo('#modalAddPeople form');
-                    return true;
-            });
 
     $("#modalAddPeople .close").on('click', function(){
                 $("#modalAddPeople").remove();
@@ -75,33 +40,10 @@ require([/*'stepa','Accordion',*/'text!../templates/addEmployee.html'], function
     function onAjaxSuccess(data){// по приходу колбэка после сохранения нового сотрудника
 
                 $("#modalAddPeople :input").val("");
-//                 $("#ajaxAddNewPeople").on('click', function(){
-//
-//                     $.post(
-//                         "/get",
-//                         {
-//                             target: 'person',
-//                             method: 'add',
-//                             name: $("#nameAddPeople").val(),
-//                             surname: $("#surnameAddPeople").val(),
-//                             position: $("#positionAddPeople").val(),
-//                             photo: $("#photoAddPeople").val()
-//
-//                         },
-//                         onAjaxSuccess
-//
-//                     );
 
              }
-//                     function onAjaxSuccess(data){
-//                         // Здесь мы получаем данные, отправленные сервером in Object{data:{Object}}.
-//                         console.log(data);
-//                 }
-//        });
 
     });
-//
-     });
 });
 
 //            var template = '<div id="modalAddPeople" class="modal"\
--- public/javascripts/requier/stepa.js
+++ public/javascripts/requier/stepa.js
@@ -2,7 +2,7 @@
  * Created by Jura on 01.12.13.
  */
 
-define(['newEmployee', 'addEmployee'/*'confirmDate', 'bootstrap-datepicker'/*,'addProject'*/],function () {
+define(['newEmployee', 'addEmployee','addProject','newProject'/*,'confirmDate', 'bootstrap-datepicker'*/],function () {
 
 
 
@@ -12,11 +12,11 @@ define(['newEmployee', 'addEmployee'/*'confirmDate', 'bootstrap-datepicker'/*,'a
 
 
 //  тест получить данные из базы
-//$.get('/get', {target: 'person', method: 'all'}, function (dataPerson) {
+//$.get('/get', {target: 'project', method: 'all'}, function (dataPerson) {
 //    var people = {};
 //    for(var i=0; i<dataPerson.length; i++){
 //        console.log(dataPerson[i]);
 //    }
 //});
-//
+
 });

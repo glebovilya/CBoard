469e1684ee3bc29d768c8ed0e79a4a0e247dfd96
glebovJs <glebovmail@gmail.com>
2013-12-05 12:36:31 +0200

    refactored person addition form, some project class updates

--- public/javascripts/requier/ilya.js
+++ public/javascripts/requier/ilya.js
@@ -4,10 +4,12 @@
 
 require(['Classes/Project'], function (Project){
     var project = new Project({name:'projectSetup'})
-    console.log(project);
-    $.post('/project',{name:project.name,startDate:project.start},function(data){
-        console.log(data)
+//    console.log(project);
+//    $.post('/project',{name:project.name,startDate:project.start},function(res){
+//        console.log(res)
+//    })
+    $.get('/users',{},function(res){
+        console.log(res);
+        project.getEmployees();
     })
-    if(null === undefined){
-    }
 });

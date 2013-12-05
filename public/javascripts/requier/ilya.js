46ac161f1ddf322833451d95f718f291abc6aea6
glebovJs <glebovmail@gmail.com>
2013-12-05 17:21:00 +0200

    life is pain

--- public/javascripts/requier/ilya.js
+++ public/javascripts/requier/ilya.js
@@ -3,13 +3,5 @@
 * */
 
 require(['Classes/Project'], function (Project){
-    var project = new Project({name:'projectSetup'})
-//    console.log(project);
-//    $.post('/project',{name:project.name,startDate:project.start},function(res){
-//        console.log(res)
-//    })
-    $.get('/users',{},function(res){
-        console.log(res);
-        project.getEmployees();
-    })
-});
+
+})

2437d0082fd35a40a46d70f3033e13d138ac8ee5
Stepanjuk <stepanjuk@nixsolutions.com>
2013-12-06 03:13:32 +0400

    modified:   public/javascripts/requier/Classes/Person.js 	modified:   public/javascripts/requier/newEmployee.js 	modified:   public/javascripts/require-jquery.js 	modified:   public/stylesheets/styleForTemplates.css

--- public/javascripts/requier/Classes/Person.js
+++ public/javascripts/requier/Classes/Person.js
@@ -1,31 +1,47 @@
 /**
  * Created by stepanjuk on 28.11.13.
  */
+define (['text!../templates/employe.html'], function(templ){
 
+    var Person ={
+        init: function(idPerson){
+                     function  onAjaxSuccess(data){
+                         data.id = id;
+                         var employee = new Person.Employee(data)
+                         Person.render(employee);
+                     }
+                     var id = idPerson;
+                     $.get("/user",{ id: id}, onAjaxSuccess);
+               },
 
-define ([], function(){
+        Employee: function (data){
+                     var idFix = Math.random().toString(36).slice(3,9);
+                     this.idFix = idFix
+                     this.domNode= "#"+idFix;
+                     this.name = data['name'];
+                     this.surname = data['surname'];
+                     this.id =data['id'];
+                     this.photo = data['photo'];
+                     this.position = data['position'];
+                    },
 
-
-
-    function Employee(idt){
-        var idFix = Math.random().toString(36).slice(3,9);
-
-        $.get("/user",{ id: idt}, onAjaxSuccess);
-
-
-        function  onAjaxSuccess(data){
-            id = "person_"+idFix;
-
-                console.log(data)
-            this.domNode = $("#" + id);
-            this.photo = data.photo;
-            this.name = data.name;
-            this.surname = data.surname;
-            this.position = data.position;
-        }
-
-
-    }
-    return Employee;
-
+        template: templ,
+        render: function(employee){
+                    var divWindow =document.createElement("div");
+                    document.body.appendChild(divWindow);
+                    divWindow.id = employee.idFix;
+                    divWindow.className = "newEmployee";
+                    $(divWindow).append(Person.template);
+                    $(employee.template).ready(function(){
+                         $(employee.domNode).find(".employee-header").append('<button type="button" class="close" data-toggle="tooltip" title="remove from project"  aria-hidden="true" >&times;</button>');
+                         $(employee.domNode).find(".united .name").html(employee.name+'<br/>'+employee.surname);
+                         $(employee.domNode).find(".emplPosition").html(employee.position);
+                         $(employee.domNode).find(".united img").attr("src", employee.photo)
+                         $(employee.domNode).find("button").on('click', function(event){
+                             $(employee.domNode).remove();
+                         });
+                     });
+                }
+    };
+    return Person;
 });
--- public/javascripts/requier/newEmployee.js
+++ public/javascripts/requier/newEmployee.js
@@ -1,99 +1,54 @@
 /**
  * Created by stepanjuk on 02.12.13.
  */
-define(['Classes/Person','text!./templates/employe.html' ], function(Employee, templ){
+define(['Classes/Person'], function(Person){
 
         function showPerson(dataId){
 
-
-                var id = dataId;
-                var idFix = Math.random().toString(36).slice(3,9);
-
-                $.get("/user",{ id: id},onAjaxSuccess);
-
-                function onAjaxSuccess(data){
-                    // Здесь мы получаем данные, отправленные сервером in Object{data:{Object}}.
-//                    console.log(data);
+                    Person.init(dataId);
 
 
-                    var divWindow =document.createElement("div");
-                    document.body.appendChild(divWindow);
-                    divWindow.style.zIndex = 99999;
-                    divWindow.style.position = "absolute";
-                    divWindow.style.top = "100px";
-                    divWindow.style.left = "500px";
-
-                    idFix = "person_"+idFix;
-                    divWindow.id = idFix;
-
-
-                    var empl = new Employee({
-                        domNode: $("#" + idFix),
-                        id:id,
-                        photo: data.photo,
-                        name: data.name,
-                        surname: data.surname,
-                        position:data.position
-                    });
-
-                    empl.template =templ;
-
-                    $(divWindow).append(empl.template);
-                    $(empl.template).ready(function(){
-
-                        $(empl.domNode).find(".employee-header").append('<button type="button" class="close" data-toggle="tooltip" title="remove from project"  aria-hidden="true" >&times;</button>');
-                        $(empl.domNode).find(".united .name").html(empl.name+'<br/>'+empl.surname);
-                        $(empl.domNode).find(".emplPosition").html(empl.position);
-                        $(empl.domNode).find(".united img").attr("src", empl.photo)
-                    });
-
-                    $(empl.domNode).find("button").on('click', function(event){
-                        $(empl.domNode).remove();
-                    });
-
-                    $(empl.domNode).attr("dataId", id);
-                   jQuery(function($){
-
-                        $(empl.domNode).drag(function( ev, dd ){
-
-
-//                            console.log($(this).attr("dataId"));
-                            $( this ).css({
-                                top: dd.offsetY,
-                                left: dd.offsetX
-                            });
-
-
-                        });
-                    });
-
-                    $(empl.domNode).drag('init',function (ev, dd) {
-                        $(dd.drag).parents('div:eq(0)').css('position', 'absolute');
-//                   $.post('/user/:id', {"target": "person", "method": "add", "name": "superTEST", "surname": "TESTSurname", "position": "gg", "photo": "dd"}, function(ell){
-
-//
-                    $.post('/user/'+$(this).attr("dataId"), {}, function(ell){
-                        console.log('setCur--->'+ ell+"id="+$(this).attr("dataId") )
-                    });
-
-                    });
-
-                    $(empl.domNode).drag('end', function (ev, dd) {
-                    $.post('/get', {target: 'person', id: 1}, function(ell){
-
-                    });
-
-                    if (dd.drop) {
-
-                        $(dd.drag).removeAttr('style');
-                        $(dd.drop).append($(dd.drag).parents('div:eq(0)').css('position', 'relative'));
-//
-                        }
-                    });
-
-                 return empl;
-            }
         }
     return showPerson
 });
 
+//
+//$(empl.domNode).attr("dataId", id);
+//jQuery(function($){
+//
+//    $(empl.domNode).drag(function( ev, dd ){
+//
+//
+////                            console.log($(this).attr("dataId"));
+//        $( this ).css({
+//            top: dd.offsetY,
+//            left: dd.offsetX
+//        });
+//
+//
+//    });
+//});
+
+//$(empl.domNode).drag('init',function (ev, dd) {
+//    $(dd.drag).parents('div:eq(0)').css('position', 'absolute');
+////                   $.post('/user/:id', {"target": "person", "method": "add", "name": "superTEST", "surname": "TESTSurname", "position": "gg", "photo": "dd"}, function(ell){
+//
+////
+//    $.post('/user/'+$(this).attr("dataId"), {}, function(ell){
+//        console.log('setCur--->'+ ell+"id="+$(this).attr("dataId") )
+//    });
+//
+//});
+//
+//$(empl.domNode).drag('end', function (ev, dd) {
+//    $.post('/get', {target: 'person', id: 1}, function(ell){
+//
+//    });
+//
+//    if (dd.drop) {
+//
+//        $(dd.drag).removeAttr('style');
+//        $(dd.drop).append($(dd.drag).parents('div:eq(0)').css('position', 'relative'));
+////
+//    }
+//});

48b4e40c6fa0a842da757149ff150cc02795cf34
glebovJs <glebovmail@gmail.com>
2013-12-07 17:25:14 +0200

    fixes into project template layout

--- public/javascripts/requier/Classes/Project.js
+++ public/javascripts/requier/Classes/Project.js
@@ -1,4 +1,4 @@
-define(['text!../templates/project.html','Classes/Person'], function (template,Person) {
+define(['text!../templates/project.html', 'Classes/Person'], function (template, Person) {
 
     var Project = function (/*string*/id) {
 
@@ -15,7 +15,7 @@ define(['text!../templates/project.html','Classes/Person'], function (template,P
         // object alias
         var self = this;
 
-        this.__construct = function (){
+        this.__construct = function () {
             this.renderView();
             this.buildLogic();
         };
@@ -26,39 +26,46 @@ define(['text!../templates/project.html','Classes/Person'], function (template,P
         };
         this.buildLogic = function () {
             //return a project record from db
-            $.ajax({
-                url:'/project',
-                async: false,
-                method: 'GET',
-                data: {id : id},
-                success : function(res){
-                    for (var i in res) {
-                        self[i] = res[i];
-                    }
-                    // временный костыль, при появлении функционала истории исчезнет сам по себе
-                    self.setDefaults();
-                    for(var i=2; i<6; i++){
-                        self.addEmployee(i);
-                    }
-                    var empl = self.currentEmployees;
-                    var devs = $('[data-role=devs]');
-                    var leads = $('[data-role=leads]');
-                    for(var i in empl){
-                        var idPerson, parentNode, id, name = empl[i].name;
-                        if(empl[i].position!='Developer'){
-                            idPerson = {id:empl[i]._id, parentNode:leads}
-                            name = Person.init(idPerson);
-                        }else{
-                            idPerson = {id:empl[i]._id, parentNode:devs}
-                            name = Person.init(idPerson);
-                        }
+//            console.log(typeof self);
+            if (!(typeof self == Object)) {
+                $.ajax({
+                        url: '/project',
+                        async: false,
+                        method: 'GET',
+                        data: {id: id},
+                        success: function (res) {
+                            for (var i in res) {
+                                self[i] = res[i];
+                            }
+                            // временный костыль, при появлении функционала истории исчезнет сам по себе
+                            self.setDefaults();
 
-                    };
+                            for (var i = 2; i < 7; i++) {
+                                self.addEmployee(i);
+                            }
 
-                }}
-            )
+                            var empl = self.currentEmployees;
+                            var devs = $('[data-role=devs]');
+                            var leads = $('[data-role=leads]');
+                            for (var i in empl) {
+                                var idPerson, parentNode, id, name = empl[i].name;
+                                if (empl[i].position != 'Developer') {
+                                    idPerson = {id: empl[i]._id, parentNode: leads}
+                                    name = Person.init(idPerson);
+                                } else {
+                                    idPerson = {id: empl[i]._id, parentNode: devs}
+                                    name = Person.init(idPerson);
+                                }
+
+                            }
+                            ;
+
+                        }}
+                )
+            }
+            ;
         };
-        this.setDefaults = function(){
+        this.setDefaults = function () {
             //      class Project defaults:
 //            alert(self);
 //            console.log();
@@ -72,20 +79,19 @@ define(['text!../templates/project.html','Classes/Person'], function (template,P
             !self.history ? self.history = [] : true;
 
         };
-        this.addEmployee = function(empId){
+        this.addEmployee = function (empId) {
             //this function adds employee, taken from db by his ID, to this project
 //            self.currentEmployees ? console.log('I have some employees!') : self.currentEmployees = [];
             $.ajax({
                 url: '/user',
-                data: {id:empId},
+                data: {id: empId},
                 async: false,
                 method: 'GET',
-                success: function(data){
+                success: function (data) {
                     self.currentEmployees.push(data);
                 }
             })
         }
-
 
 
         this.pushHistory = function () {
@@ -93,14 +99,14 @@ define(['text!../templates/project.html','Classes/Person'], function (template,P
              * this method creates a new history records
              * on drag person over the project
              * in the project and person schemas
-            */
+             */
         };
-        this.getEmployees = function(){
+        this.getEmployees = function () {
 //Закоменчено пока не разобрались с историей
 //            $.get('/project',{id:id},function(data){
 //                console.log(data);
 //            })
-            console.log (self);
+            console.log(self);
             return self.currentEmployees;
 
         };

962f2a9c72b349d5f335910c50846dfb6fedcca7
glebovJs <glebovmail@gmail.com>
2013-12-09 15:24:59 +0200

    removed automatic project window appearance

--- public/javascripts/requier/ilya.js
+++ public/javascripts/requier/ilya.js
@@ -2,8 +2,8 @@
 * This is have to be addProject.js soon
 * */
 define(['Classes/Project'], function (Project){
-    var id = 83;
-    var project = new Project(id);
+//        var id = 83;
+//        var project = new Project(id);
 //    $.get('/project',{id:2},function(data){
 //        console.log(data);
 //    })

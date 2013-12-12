2acf13d09918b1a9bcab13022b7a0dd8b7b3a1a2
Stepanjuk <stepanjuk@nixsolutions.com>
2013-12-12 15:09:45 +0200

    modified:   public/javascripts/requier/Classes/Person_new.js

--- public/javascripts/requier/Classes/Person_new.js
+++ public/javascripts/requier/Classes/Person_new.js
@@ -85,24 +85,13 @@ var Person = function(idPerson) {
                         dd.limit.bottom = dd.limit.top + $div.outerHeight() - $( this ).outerHeight();
                         dd.limit.right = dd.limit.left + $div.outerWidth() - $( this ).outerWidth();
 
-//                        console.log('start')
                         return $( this ).clone()
                             .css("opacity", .75 )
                             .appendTo( this.parentNode );
-
-
                     })
 
                     .drag("init", function(ev, dd){
-
                         dd.drop=$(".drop");
-                        console.log(dd)
-
-//                         $(dd.proxy).css({
-//                            position: 'fixed',
-//                            top: dd.offsetY,
-//                            left: dd.offsetX
-//                        })
                     })
                     .drag(function( ev, dd ){
                         $('.drop').css({
@@ -122,7 +111,6 @@ var Person = function(idPerson) {
                             borderColor: "auto"
                         })
                     });
-
 
                 $('.drop')
                     .drop(function (ev,dd){

78c38dc5868edd034295518c55a054f356cd1a52
Vlad Sinelnikov <sinelnikovwork@gmail.com>
2013-12-13 15:52:23 +0200

    fixed issue on project creating with no date

--- public/javascripts/requier/innerContainer.js
+++ public/javascripts/requier/innerContainer.js
@@ -4,7 +4,7 @@ define([], function() {
         addObj: function(obj){
             this.storage.push(obj);
         },
-        dropObj: function(obj){
+        dropObj: function(obj){ //todo make posible to drop item by !id!
             var storage = this.storage;
             var idx = storage.indexOf(obj);
             storage.splice(idx, 1);

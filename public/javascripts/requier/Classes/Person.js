35525e5386d123c4d5e5a8c9a3a9e4d896393aab
Vlad Sinelnikov <sinelnikovwork@gmail.com>
2013-12-19 13:21:50 +0200

    search fixes on board

--- public/javascripts/requier/Classes/Person.js
+++ public/javascripts/requier/Classes/Person.js
@@ -111,10 +111,9 @@ var Person = function(idPerson) {
             Person.bindDomNodes();
             var self =this;
             $(this.domNode).find("button").on('click', function(event){
-
                 if(self.projectID !== undefined){
                     for(var i in storage.storage){
-                        if(storage.storage[i].id === self.projectID && storage.storage[i].start){
+                        if(storage.storage[i].id === self.projectID && storage.storage[i].devs /*this needs to check if obj is a Project*/){
                             var re = new RegExp(self.searchName);
                             storage.storage[i].searchName = storage.storage[i].searchName.replace(re, '');
                         }
